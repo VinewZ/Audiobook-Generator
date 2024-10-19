a5fffddaa1fd8ff28d408f423ee6165c35f2bbd8
VinewZ <vinewalves@hotmail.com>
2024-10-19 16:44:09 -0300

    Extract texts from images

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require github.com/gen2brain/go-fitz v1.24.14
 require (
 	github.com/ebitengine/purego v0.8.0 // indirect
 	github.com/jupiterrider/ffi v0.2.0 // indirect
+	github.com/otiai10/gosseract/v2 v2.4.1 // indirect
 )
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"image/jpeg"
+	"image/png"
 	"log"
 	"os"
 	"path"
@@ -11,12 +11,16 @@ import (
 	"time"
 
 	"github.com/gen2brain/go-fitz"
+	"github.com/otiai10/gosseract/v2"
 )
+
+var pdfTotalPages int
 
 func main() {
 	pdfCmd := flag.NewFlagSet("pdf", flag.ExitOnError)
 	pdfName := pdfCmd.String("name", "", "Name to be saved")
 	pdfPath := pdfCmd.String("path", "", "PDF file path")
+	pdfLang := pdfCmd.String("lang", "por", "PDF language")
 
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: AllPDF <subcommand> [flags]")
@@ -37,20 +41,21 @@ func main() {
 		var option string
 		fmt.Scanln(&option)
 		if option == "y" {
-			start(*pdfPath, *pdfName)
+			start(*pdfPath, *pdfName, *pdfLang)
 		} else {
-      fmt.Println("Aborted")
-      os.Exit(1)
-    }
+			fmt.Println("Aborted")
+			os.Exit(1)
+		}
 	default:
 		fmt.Println("subcommand is required (pdf)")
 		os.Exit(1)
 	}
 }
 
-func start(src, fileName string){
-  cpFile(src, fileName)
-  convertToImgs(path.Join("./tmp", fileName), fileName)
+func start(src, fileName, pdfLang string) {
+	cpFile(src, fileName)
+	convertToImgs(path.Join("./tmp", fileName), fileName)
+	extractTexts(path.Join("./tmp", fileName), fileName, pdfLang)
 }
 
 func cpFile(src, fileName string) (dst string) {
@@ -77,41 +82,81 @@ func cpFile(src, fileName string) (dst string) {
 	return fmt.Sprintf("./tmp/%s", fileName)
 }
 
+func convertToImgs(src, fileName string) {
+	fmt.Println("Starting converting to images")
+	startTime := time.Now()
+	pdf, err := fitz.New(fmt.Sprintf("%s/%s.pdf", src, fileName))
+	if err != nil {
+		log.Fatalf("Error while opening file with Fitz: %v", err)
+	}
+	defer pdf.Close()
 
-func convertToImgs(src, fileName string){
-  fmt.Println("Starting converting to images")
-  startTime := time.Now()
-  pdf, err := fitz.New(fmt.Sprintf("%s/%s.pdf", src, fileName))
-  if err != nil {
-    log.Fatalf("Error while opening file with Fitz: %v", err)
-  }
-  defer pdf.Close()
+	imgsDirPath := path.Join(src, "imgs")
+	err = os.Mkdir(imgsDirPath, os.ModePerm)
+  pdfTotalPages = pdf.NumPage()
 
-  imgsDirPath := path.Join(src, "imgs")
-  err = os.Mkdir(imgsDirPath, os.ModePerm)
+	for i := 0; i < pdfTotalPages / 15; i++ {
 
-  for i := 0; i < pdf.NumPage() / 15; i++ {
+		fmt.Printf("Converting page %d / %d\n", i, pdfTotalPages / 15)
+		img, err := pdf.Image(i)
+		if err != nil {
+			log.Fatalf("Error while converting page Number %d, to image: %v", i, err)
+		}
 
-    fmt.Printf("Converting page %d / %d\n", i, pdf.NumPage() / 10)
-    img, err := pdf.Image(i)
-    if err != nil {
-      log.Fatalf("Error while converting page Number %d, to image: %v", i, err)
-    }
+		f, err := os.Create(filepath.Join(imgsDirPath, fmt.Sprintf("%s-%04d.png", fileName, i)))
+		if err != nil {
+			log.Fatalf("Error while saving image Number %d: %v", i, err)
+		}
+		defer f.Close()
 
-    f, err := os.Create(filepath.Join(imgsDirPath, fmt.Sprintf("%s-%04d.jpg", fileName, i)))
-    if err != nil {
-      log.Fatalf("Error while saving image Number %d: %v", i, err)
-    }
-    defer f.Close()
+		err = png.Encode(f, img)
+		if err != nil {
+			log.Fatalf("Error while encoding PNG Number %d: %v", i, err)
+		}
 
-    err = jpeg.Encode(f, img, &jpeg.Options{Quality: jpeg.DefaultQuality})
-    if err != nil {
-      log.Fatalf("Error while encoding JPEG Number %d: %v", i, err)
-    }
+	}
 
-  }
+	durationTime := time.Since(startTime)
+	fmt.Println("Finished converting to images")
+	fmt.Printf("Took %.2f seconds\n", durationTime.Seconds())
+}
 
-  durationTime := time.Since(startTime)
-  fmt.Println("Finished converting to images")
-  fmt.Printf("Took %.2f seconds\n", durationTime.Seconds())
+func extractTexts(src, fileName, pdfLang string) {
+	fmt.Println("Starting text extraction")
+	startTime := time.Now()
+
+	textsDirPath := path.Join(src, "texts")
+	err := os.Mkdir(textsDirPath, os.ModePerm)
+	if err != nil {
+		log.Fatalf("Error while creating text dir path: %v", err)
+	}
+	txtFile, err := os.OpenFile(filepath.Join(textsDirPath, fmt.Sprintf("%s.txt", fileName)), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("Error while creating text file: %v", err)
+	}
+	defer txtFile.Close()
+
+	imgsDirPath := path.Join(src, "imgs")
+	imgs, err := os.ReadDir(imgsDirPath)
+	if err != nil {
+		log.Fatalf("Erro while creating getting imgs at dir path: %v", err)
+	}
+
+	client := gosseract.NewClient()
+	client.SetLanguage(pdfLang)
+	defer client.Close()
+
+	for idx, img := range imgs {
+    fmt.Printf("Extracting page %d/%d\n", idx, pdfTotalPages / 15)
+		client.SetImage(filepath.Join(imgsDirPath, img.Name()))
+		txt, err := client.Text()
+		if err != nil {
+			log.Fatalf("Erro while extracting text from image %d: %v", idx, err)
+		}
+		txtFile.WriteString(txt)
+	}
+
+	durationTime := time.Since(startTime)
+	fmt.Println("Finished text extraction")
+	fmt.Printf("Took %.2f seconds\n", durationTime.Seconds())
 }
